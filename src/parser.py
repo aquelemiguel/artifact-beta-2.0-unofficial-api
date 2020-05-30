bca609338671e6819955669067822905703c9757
Miguel Fernandes <miguelmanofernandes@gmail.com>
2020-05-30 01:55:54 +0100

    Add parent/child references in card text and attribute ID

--- src/parser.py
+++ src/parser.py
@@ -21,17 +21,47 @@ entries = [re.match(reg, l.strip()) for l in r.splitlines()]
 entries = [entry.groups() for entry in entries if entry]
 cards = [[entries[i][0], entries[i][1], entries[i+1][1]] for i in range(0, len(entries), 2)]
 
-# Clean card text to readable format
-for card in cards:
-    card[2] = card[2].replace('{s:thisCardName}', card[1])
-    card[2] = remove_attr_syntax(card[2])
+def get_card(card_dict, identifier):
+    card_list = card_dict['card_set']['card_list']
+    
+    try: 
+        int(identifier)
+        return [card for card in card_list if card['card_id'] == identifier][0]
+    
+    except ValueError:
+        return [card for card in card_list if card['card_name']['english'] == identifier][0]
+
 
 with open('../cards.json', 'w+') as f:
     card_dict = { 'card_set': { 'version': 1, 'card_list': [] } }
     
-    card_dict['card_set']['card_list'] = [{
-        'card_id': card[0], 'card_name': { 'english': card[1] }, 'card_text': { 'english': card[2] }
-    } for card in cards]
-    
+    # Clean card text to readable format
+    for card in cards:
+        card[2] = card[2].replace('{s:thisCardName}', card[1])
+        card_text = remove_attr_syntax(card[2])
+
+        card_info = {
+            'card_id': card[0],
+            'card_name': { 'english': card[1] },
+            'card_text': { 'english': card_text },
+            'references': []
+        }
+
+        card_dict['card_set']['card_list'].append(card_info)
+
+    # Add reference to ability cards
+    for card in cards:
+        for ability in re.findall('\[abilityname\[([^\[\]]+)\]\]', card[2]):
+            
+            parent_card = get_card(card_dict, card[1])
+            child_card = get_card(card_dict, ability)
+
+            # Replace possible {s:parentCardName} references by parent name
+            new_t = child_card['card_text']['english'].replace('{s:parentCardName}', parent_card['card_name']['english'])
+            child_card['card_text']['english'] = new_t
+
+
+            parent_card['references'].append(child_card['card_id'])
+                
     f.write(json.dumps(card_dict, indent=4))
     
